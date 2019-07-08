dcdfb52708adf320340146f8b9704609ae4b124d
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-08 22:49:16 +0200

    add label for attendee_ids field

--- models/Session.py
+++ models/Session.py
@@ -12,4 +12,4 @@ class Session(models.Model):
     course_id = fields.Many2one('openacademy.course', ondelete="cascade", string="Course",
                                 required=True)
 
-    attendee_id = fields.Many2many('res.partner', string="Attendees")
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
