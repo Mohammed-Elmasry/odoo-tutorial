ba36d4b65ead3268f570941d412365586e692c97
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-08 22:23:04 +0200

    add instructor field to session model

--- models/Session.py
+++ models/Session.py
@@ -8,3 +8,5 @@ class Session(models.Model):
     start_date = fields.Datetime()
     duration = fields.Float(digits=(6,2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    instructor_id = fields.Many2one('res.partner', ondelete="set null", string="Instructor")
+    
