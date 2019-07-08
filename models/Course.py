815c0d2690239ea6f9f877f896565106561d64cf
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-08 19:23:44 +0200

    define Course model

---
+++ models/Course.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+
+class Course(models.Model):
+    _name = "openacademy.course"
+    _description = "openAcademy courses"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+
+    
