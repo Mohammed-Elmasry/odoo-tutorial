463f7c774d628543452dc3742d1a76741d433ac9
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-18 16:13:40 +0200

    create a default value for start_date field as today

--- models/Session.py
+++ models/Session.py
@@ -1,11 +1,12 @@
 from odoo import models, fields, api
+from datetime import datetime
 
 class Session(models.Model):
     _name = "openacademy.session"
     _description = "Openacademy Sessions"
 
     name = fields.Char(required=True)
-    start_date = fields.Datetime()
+    start_date = fields.Datetime(default=datetime.today())
     duration = fields.Float(digits=(6,2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string="Instructor", domain=['|', ("instructor","=",True) \
