12aff927b4f750cbb25cd87e30f52efe8c5bb3dd
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-18 15:30:27 +0200

    add a category selection field in the partner model

--- models/Partner.py
+++ models/Partner.py
@@ -4,5 +4,6 @@ class Partner(models.Model):
     _inherit = "res.partner"
 
     instructor = fields.Boolean("Instructor", default=False)
+    category = fields.Selection([("teacher1","Teacher 1"),("teacher2"),"Teacher 2"])
     session_ids = fields.Many2many("openacademy.session",string="Attended sessions"
                                    , readonly=True)
