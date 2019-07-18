c39c9a87073f67b7c5a42f66b73b66669801559b
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-18 16:17:06 +0200

    rectify the start_date record data field

--- models/Session.py
+++ models/Session.py
@@ -6,7 +6,7 @@ class Session(models.Model):
     _description = "Openacademy Sessions"
 
     name = fields.Char(required=True)
-    start_date = fields.Datetime(default=datetime.today())
+    start_date = fields.Datetime(default=datetime.today().strftime("%Y-%m-%d"))
     duration = fields.Float(digits=(6,2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string="Instructor", domain=['|', ("instructor","=",True) \
