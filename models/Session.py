a39bd06930335d314fca4999a61836dad3e213d9
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-18 16:02:50 +0200

    define computed field and function _taken_seats method in the Session.py model

--- models/Session.py
+++ models/Session.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Session(models.Model):
     _name = "openacademy.session"
@@ -14,3 +14,13 @@ class Session(models.Model):
                                 required=True)
 
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+    taken_seats = fields.Float(string="Taken seats", compute="_taken_seats")
+
+    @api.depends("attendee_ids", seats)
+    def _taken_seats(self):
+        for record in self:
+            if not record.seats:
+                record.seats = 0.0;
+            else:
+                record.taken_seats = 100.0 * len(record.attendee_ids) / record.seats
